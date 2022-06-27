c7ea5341589b942470e1fdb397d36b0566250fed
Haley Telega <haleytelega@outlook.com>
2022-06-27 17:40:59 -0400

    added about tests

---
+++ src/components/About/__tests__/index.test.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import { render, cleanup } from '@testing-library/react';
+
+import '@testing-library/jest-dom/extend-expect';
+
+import About from '..';
+
+afterEach(cleanup); //cleans up leftover memory data that could give us false results
+
+describe('About component', () => { //describe declares with component is being tested
+    //first test that
+    it('renders', () => {
+        render(<About />);
+    })
+
+    //second test
+    it('matches snapshot DOM node structure', () => {
+        const { asFragment } = render(<About />);
+        expect(asFragment()).toMatchSnapshot();
+    })
+})
