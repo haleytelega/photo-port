7eb45e1cdcd88fafdb769c7f36b08d6d8aa62194
Haley Telega <haleytelega@outlook.com>
2022-06-30 11:14:11 -0400

    added hooks

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,13 +1,16 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { capitalizeFirstLetter } from "../../utils/helpers";
 
-function Nav() {
-  const  categories = [
-    { name: 'commercial', description: 'Photos of grocery stores, food trucks, and other commercial projects' },
-    { name: 'portraits', description: 'Portraits of people in my life' },
-    { name: 'food', description: 'Delicious delicacies' },
-    { name: 'landscape', description: 'Fields, farmhouses, waterfalls, and the beauty of nature' }
-  ];
+function Nav(props) {
+  const  {
+    categories = [],
+    setCurrentCategory,
+    currentCategory,
+  } = props
+
+  useEffect(() => {
+    document.title = capitalizeFirstLetter(currentCategory.name);
+  }, [currentCategory]);
 
   const handleClick = () => {
     console.log("click handled")
@@ -17,7 +20,7 @@ function Nav() {
     <header data-testid="header" className="flex-row px-1">
       <h2>
         <a data-testid="link" href="/">
-          <span role="img" aria-label="camera"> 📸</span> Oh Snap!
+          <span role="img" aria-label="camera"> 📸</span>  Oh Snap!
         </a>
       </h2>
       <nav>
@@ -32,11 +35,15 @@ function Nav() {
               Contact
             </span>
           </li>
-          {
-            categories.map((category) => (
-              <li className="mx-1" key={category.name} >
-                <span onClick={() => { handleClick(); }}>
-                  {capitalizeFirstLetter(category.name)}
+          {categories.map((category) => (
+              <li className={`mx-1 ${
+                currentCategory.name === category.name && 'navActive'
+                }`} key={category.name}>
+              <span onClick={() => {
+                  setCurrentCategory(category)
+                }}
+              >
+                {capitalizeFirstLetter(category.name)}
                 </span>
               </li>
             ))
